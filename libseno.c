cba1633cf4ffb05afd1b8d9b3f38123cd0a12f84
Karine Valença <valenca.karine@gmail.com>
2017-05-29 19:36:18 -0300

    Calculo do seno

--- libseno.c
+++ libseno.c
@@ -5,3 +5,14 @@ int fatorial (int n) {
     else
       return fatorial(n-1)*n;
 }
+
+double seno (double angulo){
+	double seno=0, auxSeno=0;
+	int i;
+
+	for(i=0; i<10; i++){
+		auxSeno = ((-1)*pow(i)*(seno*pow(2*i+1)))/fatorial(2*i+1)
+		seno = seno + auxSeno;
+	}
+	return seno;
+}
--- libseno.h
+++ libseno.h
@@ -1,6 +1,5 @@
 #ifndef INPUT_H_INCLUDED
 #define INPUT_H_INCLUDED
-#include "triangles.h"
 
 int fatorial(int n);
 
