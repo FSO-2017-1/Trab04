e35e94d79ca785baed10430d8af71a4dc59d19f8
João Henrique <joaohenrique.p.almeida@gmail.com>
2017-05-30 02:09:57 -0300

    fixing flies

--- dinamic_lib/libseno.h
+++
@@ -1,8 +0,0 @@
-#ifndef INPUT_H_INCLUDED
-#define INPUT_H_INCLUDED
-
-int fatorial(int n);
-double seno(double angulo);
-double arc_seno(double seno);
-
-#endif
--- on_demand_lib/main.c
+++ on_demand_lib/main.c
@@ -1,16 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "libseno.h"
+#include <dlfcn.h>
+
 
 int main(int argc, char const *argv[]) {
+
+  void *handle;
+  double (*seno)(double);
+  double (*arc_seno)(double);
+  char *error;
+
+  handle = dlopen("./libseno.so",RTLD_LAZY);
+  if(!handle)
+  {
+    printf("%s\n", dlerror());
+    exit(1);
+  }
+
+  seno  = dlsym(handle, "seno");
+  if((error = dlerror()) != NULL)
+  {
+    printf("%s\n", error);
+    exit(1);
+  }
+
+  arc_seno  = dlsym(handle, "arc_seno");
+  if((error = dlerror()) != NULL)
+  {
+    printf("%s\n", error);
+    exit(1);
+  }
 
   printf("%s\n", argv[1]);
   printf("%s\n", argv[2]);
   double angulo;
 
   sscanf(argv[2], "%lf", &angulo);
-
 
   if (strcmp(argv[1], "-s")==0) {
     printf("Função em seno\n");
@@ -23,6 +49,8 @@ int main(int argc, char const *argv[]) {
     printf("arc_seno (%lf) = %lf\n", angulo, arc_senao);
   }else{
     printf("Erro na entrada de parâmetro  \n" );
-    
   }
+
+  dlclose(handle);
+
 }
