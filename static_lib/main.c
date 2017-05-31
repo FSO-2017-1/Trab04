d56489c616e6d52b878539c8651778f348fa4601
Karine Valença <valenca.karine@gmail.com>
2017-05-30 22:17:33 -0300

    modificado main para passar o valor correto ao arc e tbm retirado prints desnecessarios

--- static_lib/main.c
+++ static_lib/main.c
@@ -5,21 +5,17 @@
 
 int main(int argc, char const *argv[]) {
 
-  printf("%s\n", argv[1]);
-  printf("%s\n", argv[2]);
   double angulo;
 
   sscanf(argv[2], "%lf", &angulo);
 
 
   if (strcmp(argv[1], "-s")==0) {
-    printf("Função em seno\n");
     double senao = seno(angulo);
     printf("seno (%.2lf) = %lf\n", angulo, senao);
 
   }else if (strcmp(argv[1], "-a")==0) {
-    printf("Função em arcoseno\n");
-    double arc_senao = arc_seno(seno(angulo));
+    double arc_senao = arc_seno(angulo);
     printf("arc_seno (%lf) = %lf\n", angulo, arc_senao);
   }else{
     printf("Erro na entrada de parâmetro  \n" );
